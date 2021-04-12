9b4ba3870f6d94a019fd1a3718d69c2eb3a4df3d
simo.design <nadrine.simms@mymona.uwi.mona>
2021-04-12 00:00:45 -0400

    Builded Flask WTF-Form

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
 
+class UploadForm(FlaskForm):
+    description = TextAreaField('Description', validators=[DataRequired()])
+    photo = FileField(validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images Only!')])
